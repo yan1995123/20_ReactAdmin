d7170e67eaf21c88dafdb6fbb9d04c9b44f837e7
ylj <yanlj1995@163.com>
2020-08-27 17:01:16 +0800

    oneday

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Button , message} from 'antd';
 import './App.css';
+// 应用的跟组件
+export default class App extends Component{
+ 
+  handleClick=()=>{
+    message.success('成功了')
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render(){
+        return <div className="App"><Button type="primary" onClick={this.handleClick}>测试按钮</Button></div>
+    }
 }
-
-export default App;
